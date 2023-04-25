667c27f2d10c74b36f9e416427951356c8a4ab8a
jlrojo31 <jl.rojo@alumnos.upm.es>
2023-04-25 18:29:49 +0200

    alumno

--- app/src/main/java/com/example/tfgpictorutinas/HomeAlumno.java
+++ app/src/main/java/com/example/tfgpictorutinas/HomeAlumno.java
@@ -3,6 +3,13 @@ package com.example.tfgpictorutinas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class HomeAlumno extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class HomeAlumno extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_alumno);
+
+        String day = "";
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            String aux = LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL,new Locale("es","ES"));
+            day = aux.toUpperCase().charAt(0) + aux.substring(1, aux.length());
+        }
+        TextView diaActual = (TextView) findViewById(R.id.idAlumnoTV);
+        diaActual.setText(day);
     }
 }
