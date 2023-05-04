d37e3da0b6578b6347a5b2a3effdc9e82db7e870
jlrojo31 <jl.rojo@alumnos.upm.es>
2023-05-04 18:57:58 +0200

    eliminar rutinas

--- app/src/main/java/com/example/tfgpictorutinas/RutinaDef.java
+++ app/src/main/java/com/example/tfgpictorutinas/RutinaDef.java
@@ -1,9 +1,12 @@
 package com.example.tfgpictorutinas;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -13,8 +16,12 @@ import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
@@ -129,6 +136,31 @@ public class RutinaDef extends AppCompatActivity {
                     return true;
                 }
                 return false;
+            }
+        });
+
+        Button eliminarRutinaBtn = findViewById(R.id.idBtEliminarRutina);
+
+        // onclick listener para el boton de eliminar rutina
+        eliminarRutinaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Query query = myRef.child(String.valueOf(idRutina));
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        dataSnapshot.getRef().removeValue();
+
+                        Intent i = new Intent(RutinaDef.this, HomeActivity.class);
+                        Bundle extras = getIntent().getExtras();
+                        startActivity(i);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.e(TAG, "onCancelled", databaseError.toException());
+                    }
+                });
             }
         });
 
