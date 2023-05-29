bb160e2202c62a992e9f4a7a5dad9c08ec9a0785
jlrojo31 <jl.rojo@alumnos.upm.es>
2023-05-29 18:43:07 +0200

    pantalla horarios

--- app/src/main/java/com/example/tfgpictorutinas/AdaptadorHorariosAlumno.java
+++ app/src/main/java/com/example/tfgpictorutinas/AdaptadorHorariosAlumno.java
@@ -69,7 +69,7 @@ public class AdaptadorHorariosAlumno extends BaseAdapter {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         vi = inflater.inflate(R.layout.item_horario_alumno, null);
 
-
+        holder.textViewR = (TextView) vi.findViewById(R.id.idRutHor);
         holder.textViewL = (TextView) vi.findViewById(R.id.idLunes);
         holder.textViewM = (TextView) vi.findViewById(R.id.idMartes);
         holder.textViewX = (TextView) vi.findViewById(R.id.idMiercoles);
@@ -77,44 +77,45 @@ public class AdaptadorHorariosAlumno extends BaseAdapter {
         holder.textViewV = (TextView) vi.findViewById(R.id.idViernes);
         holder.textViewS = (TextView) vi.findViewById(R.id.idSabado);
         holder.textViewD = (TextView) vi.findViewById(R.id.idDomingo);
+        holder.textViewR.setText(rutina.get(position));
         if (dias.get(position).contains("L")){
-            holder.textViewL.setText(rutina.get(position));
+            holder.textViewL.setText("X");
             holder.textViewL.setTag(position);
             int tag_TextViewL_position=(Integer) holder.textViewL.getTag();
             holder.textViewL.setId(tag_TextViewL_position);
         }
         if (dias.get(position).contains("M")){
-            holder.textViewM.setText(rutina.get(position));
+            holder.textViewM.setText("X");
             holder.textViewM.setTag(position);
             int tag_TextViewM_position=(Integer) holder.textViewM.getTag();
             holder.textViewM.setId(tag_TextViewM_position);
         }
         if (dias.get(position).contains("X")){
-            holder.textViewX.setText(rutina.get(position));
+            holder.textViewX.setText("X");
             holder.textViewX.setTag(position);
             int tag_TextViewX_position=(Integer) holder.textViewX.getTag();
             holder.textViewX.setId(tag_TextViewX_position);
         }
         if (dias.get(position).contains("J")){
-            holder.textViewJ.setText(rutina.get(position));
+            holder.textViewJ.setText("X");
             holder.textViewJ.setTag(position);
             int tag_TextViewJ_position=(Integer) holder.textViewJ.getTag();
             holder.textViewJ.setId(tag_TextViewJ_position);
         }
         if (dias.get(position).contains("V")){
-            holder.textViewV.setText(rutina.get(position));
+            holder.textViewV.setText("X");
             holder.textViewV.setTag(position);
             int tag_TextViewV_position=(Integer) holder.textViewV.getTag();
             holder.textViewV.setId(tag_TextViewV_position);
         }
         if (dias.get(position).contains("S")){
-            holder.textViewS.setText(rutina.get(position));
+            holder.textViewS.setText("X");
             holder.textViewS.setTag(position);
             int tag_TextViewS_position=(Integer) holder.textViewS.getTag();
             holder.textViewS.setId(tag_TextViewS_position);
         }
         if (dias.get(position).contains("D")){
-            holder.textViewD.setText(rutina.get(position));
+            holder.textViewD.setText("X");
             holder.textViewD.setTag(position);
             int tag_TextViewD_position=(Integer) holder.textViewD.getTag();
             holder.textViewD.setId(tag_TextViewD_position);
@@ -128,6 +129,7 @@ public class AdaptadorHorariosAlumno extends BaseAdapter {
 
 class ViewHolderDias {
 
+    TextView textViewR;
     TextView textViewL;
     TextView textViewM;
     TextView textViewX;
